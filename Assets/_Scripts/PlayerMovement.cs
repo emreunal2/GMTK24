8d19b159cba58070d203789ec951d6965c0e1c73
Orhan Unal <140164675+OrhanUnal@users.noreply.github.com>
2024-09-04 14:22:10 +0300

    Camera

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] bool isFacingRight = false;
     [SerializeField]float horizontalMovement, verticalMovement;
     [SerializeField]float moveSpeed;
     [SerializeField]float jumpForce;
@@ -35,7 +36,38 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         isOnGround = Physics2D.OverlapCircle(groundCheck.transform.position, 0.2f, groundLayer);
+        if (moveInput != 0)
+        {
+            TurnCheck();
+        }
+    }
 
+    private void TurnCheck()
+    {
+        if (moveInput > 0 && !isFacingRight)
+        {
+            Turn();
+        }
+        else if (moveInput < 0 && isFacingRight)
+        {
+            Turn();
+        }
+    }
+
+    private void Turn()
+    {
+        if (isFacingRight)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = false;
+        }
+        else 
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = true;
+        }
     }
 }
  
